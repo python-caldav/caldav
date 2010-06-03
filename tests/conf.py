b19873e0d70f1db48aeaa9d47987b7f14ba3d5af
Cyril <cyrilrbt@users.noreply.github.com>
2010-06-03 14:12:17 -0400

    Version plus ou moins stable

--- tests/conf.py
+++ tests/conf.py
@@ -2,3 +2,4 @@
 # -*- encoding: utf-8 -*-
 
 principal_url = "http://sogo1:sogo1@sogo-demo.inverse.ca:80/SOGo/dav/sogo1/Calendar/"
+principal_url_ssl = "https://sogo1:sogo1@sogo-demo.inverse.ca:443/SOGo/dav/sogo1/Calendar/"
--- tests/test_caldav.py
+++ tests/test_caldav.py
@@ -1,28 +1,86 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_not_equal
 
-from conf import principal_url
+from conf import principal_url, principal_url_ssl
 
-from caldav.caldav import CalDAV
-from caldav.objects import Principal
+from caldav.davclient import DAVClient
+from caldav.objects import Principal, Calendar, Event
 from caldav.utils import namespace
+
+
+ev1 = """BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Example Corp.//CalDAV Client//EN
+BEGIN:VEVENT
+UID:20010712T182145Z-123401@example.com
+DTSTAMP:20060712T182145Z
+DTSTART:20060714T170000Z
+DTEND:20060715T040000Z
+SUMMARY:Bastille Day Party
+END:VEVENT
+END:VCALENDAR
+"""
+
+ev2 = """BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Example Corp.//CalDAV Client//EN
+BEGIN:VEVENT
+UID:20010712T182145Z-123401@example.com
+DTSTAMP:20070712T182145Z
+DTSTART:20070714T170000Z
+DTEND:20070715T040000Z
+SUMMARY:Bastille Day Party +1year
+END:VEVENT
+END:VCALENDAR
+"""
+
+
 
 class TestCalDAV:
     def setup(self):
-        self.caldav = CalDAV(principal_url)
+        self.caldav = DAVClient(principal_url)
+        self.p = Principal(self.caldav, principal_url)
 
     def teardown(self):
         pass
 
+    def testSSL(self):
+        c = DAVClient(principal_url_ssl)
+
     def testPrincipal(self):
-        p = Principal(principal_url)
-        assert_equal(p.geturl(), principal_url)
+        assert_equal(self.p.geturl(), principal_url)
 
-        collections = self.caldav.children(p, namespace.ns("D", "collection"))
+        collections = self.p.calendars()
         for c in collections:
-            assert_equal(c.__class__.__name__, "Collection")
+            assert_equal(c.__class__.__name__, "Calendar")
 
-    def testYoupi(self):
-        print "yay"
+    def testCalendar(self):
+        c = Calendar(self.caldav, name="Yep", parent = self.p).save()
+        assert_not_equal(c.url, None)
+        print c
+        # TODO: fail
+        #props = c.properties([("D", "displayname"),])
+        #assert_equal("Yep", props["{DAV:}displayname"])
+
+        e = Event(self.caldav, data = ev1, parent = c).save()
+        assert_not_equal(e.url, None)
+        print e
+
+        ee = Event(self.caldav, url = e.geturl(), parent = c)
+        ee.load()
+        assert_equal(e.instance.vevent.uid, ee.instance.vevent.uid)
+
+        r = c.date_search("20060713T170000Z", "20060715T170000Z")
+        assert_equal(e.instance.vevent.uid, r[0].instance.vevent.uid)
+        assert_equal(len(r), 1)
+
+        all = c.events()
+        assert_equal(len(all), 1)
+
+        e2 = Event(self.caldav, data = ev2, parent = c).save()
+        assert_not_equal(e.url, None)
+
+        r = c.date_search("20060713T170000Z", "20060715T170000Z")
+        assert_equal(len(r), 1)
