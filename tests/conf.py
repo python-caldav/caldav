8ffd3f60006ac53c29e6bc2c14bd918906e255a2
Tobias Brox <tobias@redpill-linpro.com>
2013-10-09 00:43:41 +0200

    set up a new testing server

--- tests/conf.py
+++ tests/conf.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-principal_url = "http://sogo1:sogo1@sogo-demo.inverse.ca:80/SOGo/dav/sogo1/Calendar/"
+principal_url = "http://testlogin:testing123@baikal.tobixen.no/cal.php/principals/testlogin
+#principal_url = "http://sogo1:sogo1@sogo-demo.inverse.ca:80/SOGo/dav/sogo1/Calendar/"
 principal_url_ssl = "https://sogo1:sogo1@sogo-demo.inverse.ca:443/SOGo/dav/sogo1/Calendar/"
 
 proxy = "127.0.0.1:8080"
