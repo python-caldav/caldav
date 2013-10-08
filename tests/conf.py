3d3b1927e84c12afc42a8536537f4ff36ae2966f
Cyril Robert <cyrilrbt@gmail.com>
2013-10-08 19:37:58 -0400

    Fixed principal_url for tests

--- tests/conf.py
+++ tests/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-principal_url = "http://testlogin:testing123@baikal.tobixen.no/cal.php/principals/testlogin
+principal_url = "http://testlogin:testing123@baikal.tobixen.no:80/cal.php/principals/testlogin"
 #principal_url = "http://sogo1:sogo1@sogo-demo.inverse.ca:80/SOGo/dav/sogo1/Calendar/"
 principal_url_ssl = "https://sogo1:sogo1@sogo-demo.inverse.ca:443/SOGo/dav/sogo1/Calendar/"
 
