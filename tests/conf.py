49a0abe3fe393dcbbba3b889d1f1c5df5ea33694
Tobias Brox <tobias@redpill-linpro.com>
2013-10-10 02:11:45 +0200

    major problems with the baikal server, commenting it out as for now

--- tests/conf.py
+++ tests/conf.py
@@ -8,7 +8,7 @@ except:
 
 caldav_urls.append("http://sogo1:sogo1@sogo-demo.inverse.ca:80/SOGo/dav/sogo1/Calendar/")
 caldav_urls.append("https://sogo1:sogo1@sogo-demo.inverse.ca:443/SOGo/dav/sogo1/Calendar/")
-caldav_urls.append("http://testlogin:testing123@baikal.tobixen.no:80/cal.php/")
+#caldav_urls.append("http://testlogin:testing123@baikal.tobixen.no:80/cal.php/")
 
 proxy = "127.0.0.1:8080"
 proxy_noport = "127.0.0.1"
