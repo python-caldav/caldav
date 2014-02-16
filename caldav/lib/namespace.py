75fa431ad913c480911f759bf747a83d8cf045f0
Tobias Brox <tobixen@gmail.com>
2014-02-16 18:31:05 +0100

    comment fix

--- caldav/lib/namespace.py
+++ caldav/lib/namespace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+## nsmap2 is ref https://bitbucket.org/cyrilrbt/caldav/issue/29/centos59-minifix
+## This looks wrong - should think more about it at a later stage. 
+## -- Tobias Brox, 2014-02-16
 
 nsmap = {
     "D": "DAV",
