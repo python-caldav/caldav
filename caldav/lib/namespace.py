4d506f528aaf2201e4ba5c98b049d7249a8fde4c
Tobias Brox <tobixen@gmail.com>
2014-02-16 18:27:05 +0100

    ref https://bitbucket.org/cyrilrbt/caldav/issue/29/centos59-minifix

--- caldav/lib/namespace.py
+++ caldav/lib/namespace.py
@@ -3,13 +3,18 @@
 
 
 nsmap = {
+    "D": "DAV",
+    "C": "urn:ietf:params:xml:ns:caldav",
+}
+
+nsmap2 = {
     "D": "DAV:",
     "C": "urn:ietf:params:xml:ns:caldav",
 }
 
 
 def ns(prefix, tag=None):
-    name = "{%s}" % nsmap[prefix]
+    name = "{%s}" % nsmap2[prefix]
     if tag is not None:
         name = "%s%s" % (name, tag)
     return name
