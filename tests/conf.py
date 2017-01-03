5ea11ad32beaed0006e1845785e4b4fb2ba53771
Tobias Brox <tobias@redpill-linpro.com>
2017-01-03 22:59:38 +0100

    new test server

--- tests/conf.py
+++ tests/conf.py
@@ -7,10 +7,12 @@ try:
 except ImportError:
     caldav_servers = []
 
-# caldav_servers.append({"url": "http://sogo1:sogo1@sogo-demo.inverse.ca:80/SOGo/dav/", "principal_url": "http://sogo-demo.inverse.ca:80/SOGo/dav/sogo1/", "backwards_compatibility_url": "http://sogo1:sogo1@sogo-demo.inverse.ca:80/SOGo/dav/sogo1/Calendar/"})
-# caldav_servers.append({"url": "https://sogo1:sogo1@sogo-demo.inverse.ca:443/SOGo/dav/sogo1/Calendar/"})
+## This one will eventually go the way of the dodo at some future point
 caldav_servers.append({"url": "http://baikal.tobixen.no:80/cal.php/", "username": "testlogin", "password": "testing123", "principal_url": "http://baikal.tobixen.no:80/cal.php/principals/testlogin/", "backwards_compatibility_url": "http://testlogin:testing123@baikal.tobixen.no:80/cal.php/calendars/testlogin/"})
-#caldav_servers.append({"url": "http://veryveryveeeeerylongusernametest:thispasswordismaybenotlongenoughbutmaybemaybemaybeitislongenoughafterallhowlongdoesatoolongpasswordneedtobeiwonder@baikal.tobixen.no/cal.php"})
+
+## newer version of baikal, running under openshift.
+caldav_servers.append({"url": "http://baikal-test.caldav-servers.tobixen.no/cal.php/", "username": "testuser", "password": "123"})
+caldav_servers.append({"url": "https://baikal-test.caldav-servers.tobixen.no/cal.php/", "username": "testuser", "password": "123", "ssl_verify_cert": False})
 
 proxy = "127.0.0.1:8080"
 proxy_noport = "127.0.0.1"
