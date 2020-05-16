3202fb401a8b60e7c56000019f80af51837a52ec
Tobias Brox <tobias@redpill-linpro.com>
2020-05-16 16:14:54 +0200

    this file is now the official collection of known server incompatibilities.  Ref https://github.com/python-caldav/caldav/issues/45

--- tests/compatibility_issues.py
+++ tests/compatibility_issues.py
@@ -29,9 +29,34 @@ zimbra = [
     ## apparently, zimbra has no journal support
     'nojournal',
 
-    ## setting display name in zimbra does not work
+    ## setting display name in zimbra does not work (display name,
+    ## calendar-ID and URL is the same, the display name cannot be
+    ## changed, it can only be given if no calendar-ID is given.  In
+    ## earlier versions of Zimbra display-name could be changed, but
+    ## then the calendar would not be available on the old URL
+    ## anymore)
     'nodisplayname'
 
-    ## TODO: there is more.  Search for 'zimbra' in the code repository!
+    ## TODO: there is more, it should be organized and moved here.
+    ## Search for 'zimbra' in the code repository!
+]
+
+bedework = [
+    ## quite a lot of things were missing in Bedework last I checked -
+    ## but that's quite a while ago!
+    'nojournal',
+    'notodo',
+    'nopropfind',
+    'norecurring'
+]
+
+baikal = [
+    ## Quite a while since I tested towards baikal, but no issues were found
+]
+
+## The version of davical I'm testing towards is very old, so this list may be outdated
+davical = [
+    'nocalendarnotfound',
+    'nojournal'
 ]
 
