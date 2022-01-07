f1c234e86f4c866b10b00e7440084cc542838414
Tobias Brox <tobias@redpill-linpro.com>
2022-01-07 22:22:41 +0100

    new test for to_wire

---
+++ tests/test_utils.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from caldav.lib.python_utilities import to_wire
+
+class TestUtils(TestCase):
+    def test_to_wire(self):
+        self.assertEqual(to_wire('blatti'), b'blatti')
+        self.assertEqual(to_wire(b'blatti'), b'blatti')
+        self.assertEqual(to_wire(u'blatti'), b'blatti')
+        self.assertEqual(to_wire(''), b'')
+        self.assertEqual(to_wire(u''), b'')
+        self.assertEqual(to_wire(b''), b'')
+        self.assertEqual(to_wire(None), None)
+
