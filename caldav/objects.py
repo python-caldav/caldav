100da7a676af2cdab12ff8c678d24e0a6dff0a38
Tobias Brox <tobias@redpill-linpro.com>
2023-01-15 20:28:41 +0100

    upcoming basic_examples.py requires len() on the syncollection thing as well

--- caldav/objects.py
+++ caldav/objects.py
@@ -1615,6 +1615,9 @@ class SynchronizableCalendarObjectCollection(object):
     def __iter__(self):
         return self.objects.__iter__()
 
+    def __len__(self):
+        return len(self.objects)
+
     def objects_by_url(self):
         """
         returns a dict of the contents of the SynchronizableCalendarObjectCollection, URLs -> objects.
