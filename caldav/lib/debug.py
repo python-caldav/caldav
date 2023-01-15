d5a61fc0653c3880971600b76f7cb0d9af5de827
Tobias Brox <tobias@redpill-linpro.com>
2023-01-15 04:34:16 +0100

    style

--- caldav/lib/debug.py
+++ caldav/lib/debug.py
@@ -2,9 +2,10 @@ from lxml import etree
 
 
 def xmlstring(root):
-    if hasattr(root, 'xmlelement'):
+    if hasattr(root, "xmlelement"):
         root = root.xmlelement()
     return etree.tostring(root, pretty_print=True).decode("utf-8")
+
 
 def printxml(root):
     print(xmlstring(root))
