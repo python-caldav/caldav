a6c39da205b419ea441820fe70e11f2444629f2d
Tobias Brox <tobias@redpill-linpro.com>
2025-05-21 22:58:19 +0200

    Reverting the niquests library - release version 1.5 should not introduce such dependency-changes

--- caldav/davclient.py
+++ caldav/davclient.py
@@ -13,12 +13,12 @@ from typing import TYPE_CHECKING
 from typing import Union
 from urllib.parse import unquote
 
-import niquests
+import requests
 from lxml import etree
 from lxml.etree import _Element
-from niquests.auth import AuthBase
-from niquests.models import Response
-from niquests.structures import CaseInsensitiveDict
+from requests.auth import AuthBase
+from requests.models import Response
+from requests.structures import CaseInsensitiveDict
 
 from .elements.base import BaseElement
 from caldav import __version__
@@ -402,7 +402,7 @@ class DAVResponse:
 
 class DAVClient:
     """
-    Basic client for webdav, uses the niquests lib; gives access to
+    Basic client for webdav, uses the requests lib; gives access to
     low-level operations towards the caldav server.
 
     Unless you have special needs, you should probably care most about
@@ -432,18 +432,18 @@ class DAVClient:
          * url: A fully qualified url: `scheme://user:pass@hostname:port`
          * proxy: A string defining a proxy server: `hostname:port`
          * username and password should be passed as arguments or in the URL
-         * auth, timeout and ssl_verify_cert are passed to niquests.request.
+         * auth, timeout and ssl_verify_cert are passed to requests.request.
          * ssl_verify_cert can be the path of a CA-bundle or False.
          * huge_tree: boolean, enable XMLParser huge_tree to handle big events, beware
            of security issues, see : https://lxml.de/api/lxml.etree.XMLParser-class.html
 
-        The niquests library will honor a .netrc-file, if such a file exists
+        The requests library will honor a .netrc-file, if such a file exists
         username and password may be omitted.  Known bug: .netrc is honored
         even if a username/password is given, ref https://github.com/python-caldav/caldav/issues/206
         """
         headers = headers or {}
 
-        self.session = niquests.Session(multiplexed=True)
+        self.session = requests.Session()
 
         log.debug("url: " + str(url))
         self.url = URL.objectify(url)
@@ -451,7 +451,7 @@ class DAVClient:
         # Prepare proxy info
         if proxy is not None:
             _proxy = proxy
-            # niquests library expects the proxy url to have a scheme
+            # requests library expects the proxy url to have a scheme
             if "://" not in proxy:
                 _proxy = self.url.scheme + "://" + proxy
 
@@ -747,11 +747,10 @@ class DAVClient:
             and self.username
         ):
             auth_types = self.extract_auth_types(r_headers["WWW-Authenticate"])
-
             if self.username and "digest" in auth_types:
-                self.auth = niquests.auth.HTTPDigestAuth(self.username, self.password)
+                self.auth = requests.auth.HTTPDigestAuth(self.username, self.password)
             elif self.username and "basic" in auth_types:
-                self.auth = niquests.auth.HTTPBasicAuth(self.username, self.password)
+                self.auth = requests.auth.HTTPBasicAuth(self.username, self.password)
             elif self.password and "bearer" in auth_types:
                 self.auth = HTTPBearerAuth(self.password)
             elif "bearer" in auth_types:
@@ -785,11 +784,11 @@ class DAVClient:
             auth_types = self.extract_auth_types(r_headers["WWW-Authenticate"])
 
             if self.password and self.username and "digest" in auth_types:
-                self.auth = niquests.auth.HTTPDigestAuth(
+                self.auth = requests.auth.HTTPDigestAuth(
                     self.username, self.password.decode()
                 )
             elif self.password and self.username and "basic" in auth_types:
-                self.auth = niquests.auth.HTTPBasicAuth(
+                self.auth = requests.auth.HTTPBasicAuth(
                     self.username, self.password.decode()
                 )
             elif self.password and "bearer" in auth_types:
@@ -801,8 +800,8 @@ class DAVClient:
 
         # this is an error condition that should be raised to the application
         if (
-            response.status == niquests.codes.forbidden
-            or response.status == niquests.codes.unauthorized
+            response.status == requests.codes.forbidden
+            or response.status == requests.codes.unauthorized
         ):
             try:
                 reason = response.reason
