d93bdfa1fa85d09458f1ea7ab422a6c2f4f25fec
Tobias Brox <tobias@redpill-linpro.com>
2025-06-15 23:21:49 +0200

    style

--- caldav/config.py
+++ caldav/config.py
@@ -107,9 +107,9 @@ def read_config(fn, interactive_error=False):
                         f"config file {fn} exists but is neither valid json nor yaml.  Check the syntax."
                     )
             except ImportError:
-                    logging.error(
-                        f"config file {fn} exists but is not valid json, and pyyaml is not installed."
-                    )
+                logging.error(
+                    f"config file {fn} exists but is not valid json, and pyyaml is not installed."
+                )
 
     except FileNotFoundError:
         ## File not found
