python-caldav/caldav

commit 507b080d02d0f7567347a0c5f6b481a8bc791b27

compatibility hints - refactoring efforts
In 1.5.0, I moved the compability matrix from the tests directory and into the project itself - now I'm doing a major overhaul of it.  This change is not much visible for end users yet - but already now it's possible to configure "compatibility hints" when setting up the davclient, and the idea is that different kind of workarounds may be applied depending on the compatibility-matrix.  Search without comp-type is wonky on many servers, now the `search`-method will automatically deliver a union of a search of the three different comp-types if a comp-type is not set in the parameters *and* it's declared that the compatibility matrix does not work.  In parallel I'm developing a stand-alone tool caldav-server-tester to check the compatibility of a caldav server.

This is "work in progress", the rewriting of old compatibility hints is incomplete.  It's a lot of work, so it will take some time to fully complete it.

caldav/collection.py
@@ -763,7 +763,7 @@ class Calendar(DAVObject):
         xml=None,
         comp_class: Optional[_CC] = None,
         todo: Optional[bool] = None,
-        include_completed: bool = False,
+        include_completed: bool = None,
         sort_keys: Sequence[str] = (),
         sort_reverse: bool = False,
         expand: bool = False,
@@ -860,21 +860,18 @@ class Calendar(DAVObject):
         if todo and not include_completed:
             matches1 = self.search(
                 todo=True,
-                comp_class=comp_class,
                 ignore_completed1=True,
                 include_completed=True,
                 **kwargs,
             )
             matches2 = self.search(
                 todo=True,
-                comp_class=comp_class,
                 ignore_completed2=True,
                 include_completed=True,
                 **kwargs,
             )
             matches3 = self.search(
                 todo=True,
-                comp_class=comp_class,
                 ignore_completed3=True,
                 include_completed=True,
                 **kwargs,
@@ -903,30 +900,48 @@ class Calendar(DAVObject):
                 raise error.ConsistencyError(
                     "Inconsistent usage parameters: xml together with other search options"
                 )
+
+            ## For some of the workarounds below, we will do a recursive search, with all
+            ## those arguments:
+            kwargs2 = {
+                "include_completed": include_completed,
+                "sort_reverse": sort_reverse,
+                "expand": expand,
+                "server_expand": server_expand,
+                "split_expanded": split_expanded,
+                "props": props,
+            }
+
+            if not comp_class and not self.client.features.check_support(
+                "search.comp-type-optional"
+            ):
+                if kwargs2["include_completed"] is None:
+                    kwargs2["include_completed"] = True
+                objects = (
+                    self.search(event=True, **kwargs2, **kwargs)
+                    + self.search(todo=True, **kwargs2, **kwargs)
+                    + self.search(journal=True, **kwargs2, **kwargs)
+                )
+                self.sort_objects(objects, sort_keys, sort_reverse)
+                return objects
+
             try:
                 (response, objects) = self._request_report_build_resultlist(
                     xml, comp_class, props=props
                 )
+
             except error.ReportError as err:
-                ## Hack for some calendar servers
-                ## yielding 400 if the search does not include compclass.
-                ## Partial fix for https://github.com/python-caldav/caldav/issues/401
-                ## This assumes the client actually wants events and not tasks
-                ## The calendar server in question did not support tasks
-                ## However the most correct would probably be to join
-                ## events, tasks and journals.
-                ## TODO: we need server compatibility hints!
-                ## https://github.com/python-caldav/caldav/issues/402
-                if not comp_class and not "400" in err.reason:
+                ## This is only for backward compatibility.  The logic is even flawed.
+                ## But it does partially fix https://github.com/python-caldav/caldav/issues/401
+                if (
+                    self.client.features.backward_compatibility_mode
+                    and not comp_class
+                    and not "400" in err.reason
+                ):
                     return self.search(
-                        event=True,
-                        include_completed=include_completed,
                         sort_keys=sort_keys,
                         sort_reverse=sort_reverse,
-                        expand=expand,
-                        server_expand=server_expand,
-                        split_expanded=split_expanded,
-                        props=props,
+                        *kwargs2,
                         **kwargs,
                     )
                 raise
@@ -976,6 +991,17 @@ class Calendar(DAVObject):
             for o in objects_:
                 objects.extend(o.split_expanded())
 
+        ## partial workaround for https://github.com/python-caldav/caldav/issues/201
+        for obj in objects:
+            try:
+                obj.load(only_if_unloaded=True)
+            except:
+                pass
+
+        self.sort_objects(objects, sort_keys, sort_reverse)
+        return objects
+
+    def sort_objects(self, objects, sort_keys, sort_reverse):
         def sort_key_func(x):
             ret = []
             comp = x.icalendar_component
@@ -1003,7 +1029,6 @@ class Calendar(DAVObject):
                     > datetime.now().strftime("%F%H%M%S")
                 ),
             }
-            ## ref https://github.com/python-caldav/caldav/issues/448 - allow strings instead of a sequence here
             for sort_key in sort_keys:
                 val = comp.get(sort_key, None)
                 if val is None:
@@ -1020,18 +1045,10 @@ class Calendar(DAVObject):
             return ret
 
         if sort_keys:
+            ## ref https://github.com/python-caldav/caldav/issues/448 - allow strings instead of a sequence here
             if isinstance(sort_keys, str):
                 sort_keys = (sort_keys,)
             objects.sort(key=sort_key_func, reverse=sort_reverse)
-
-        ## partial workaround for https://github.com/python-caldav/caldav/issues/201
-        for obj in objects:
-            try:
-                obj.load(only_if_unloaded=True)
-            except:
-                pass
-
-        return objects
 
     def build_search_xml_query(
         self,

caldav/davclient.py
@@ -26,6 +26,7 @@ from caldav import __version__
 from caldav.collection import Calendar
 from caldav.collection import CalendarSet
 from caldav.collection import Principal
+from caldav.compatibility_hints import FeatureSet
 from caldav.elements import cdav
 from caldav.elements import dav
 from caldav.lib import error
@@ -458,6 +459,7 @@ class DAVClient:
         ssl_cert: Union[str, Tuple[str, str], None] = None,
         headers: Mapping[str, str] = None,
         huge_tree: bool = False,
+        features: Union[FeatureSet, dict] = None,
     ) -> None:
         """
         Sets up a HTTPConnection object towards the server in the url.
@@ -471,6 +473,7 @@ class DAVClient:
            server etc.
           ssl_verify_cert can be the path of a CA-bundle or False.
           huge_tree: boolean, enable XMLParser huge_tree to handle big events, beware of security issues, see : https://lxml.de/api/lxml.etree.XMLParser-class.html
+          features: The default, None, will in version 2.x enable all existing workarounds in the code for backward compability.  Otherwise it will expect a FeatureSet or a dict as defined in `caldav.compatibility_hints` and use that to figure out what workarounds are needed.
 
         The niquests library will honor a .netrc-file, if such a file exists
         username and password may be omitted.
@@ -491,6 +494,7 @@ class DAVClient:
         log.debug("url: " + str(url))
         self.url = URL.objectify(url)
         self.huge_tree = huge_tree
+        self.features = FeatureSet(features)
         # Prepare proxy info
         if proxy is not None:
             _proxy = proxy
@@ -1087,19 +1091,21 @@ def get_davclient(
         try:
             from conf import client
 
-            idx = None
-            if name:
+            idx = os.environ.get("PYTHON_CALDAV_TEST_SERVER_IDX")
+            try:
+                idx = int(idx)
+            except (ValueError, TypeError):
+                idx = None
+            name = name or os.environ.get("PYTHON_CALDAV_TEST_SERVER_NAME")
+            if name and not idx:
                 try:
                     idx = int(name)
                     name = None
                 except ValueError:
                     pass
-            try:
-                conn = client(idx, name)
-                if conn:
-                    return conn
-            except:
-                error.weirdness("traceback from client()")
+            conn = client(idx, name)
+            if conn:
+                return conn
         except ImportError:
             pass
         finally:

pyproject.toml
@@ -55,7 +55,8 @@ test = [
   "tzlocal",
   "xandikos",
   "radicale",
-  "pyfakefs"
+  "pyfakefs",
+  #"caldav_server_tester"
 ]
 [tool.setuptools_scm]
 write_to = "caldav/_version.py"

tests/conf.py
@@ -11,6 +11,7 @@ import time
 import niquests
 
 from caldav import compatibility_hints
+from caldav.compatibility_hints import FeatureSet
 from caldav.davclient import CONNKEYS
 from caldav.davclient import DAVClient
 
@@ -144,7 +145,7 @@ if test_radicale:
             "username": "user1",
             "password": "",
             "backwards_compatibility_url": url + "user1",
-            "incompatibilities": compatibility_hints.radicale,
+            "features": compatibility_hints.radicale,
             "setup": setup_radicale,
             "teardown": teardown_radicale,
         }
@@ -229,7 +230,7 @@ if test_xandikos:
             "name": "LocalXandikos",
             "url": url,
             "backwards_compatibility_url": url + "sometestuser",
-            "incompatibilities": compatibility_hints.xandikos,
+            "features": compatibility_hints.xandikos,
             "setup": setup_xandikos,
             "teardown": teardown_xandikos,
         }
@@ -257,6 +258,7 @@ def client(
     elif no_args:
         return None
     for bad_param in (
+        "features",
         "incompatibilities",
         "backwards_compatibility_url",
         "principal_url",
@@ -274,7 +276,7 @@ def client(
     conn = DAVClient(**kwargs_)
     conn.setup = setup
     conn.teardown = teardown
-    conn.incompatibilities = kwargs.get("incompatibilities")
+    conn.features = FeatureSet(kwargs.get("features"))
     conn.server_name = name
     return conn
 

tests/conf_private.py.EXAMPLE
@@ -30,8 +30,8 @@ caldav_servers = [
         ## incompatibilities is a list of flags that can be set for
         ## skipping (parts) of certain tests.  See
         ## compatibility_hints.py for premade lists
-        #'incompatibilities': compatibility_hints.nextcloud
-      	'incompatibilities': [],
+        #'features': compatibility_hints.nextcloud
+      	'features': [],
 
         ## You may even add setup and teardown methods to set up
         ## and rig down the calendar server

commit 70374da2ed94d8f4db06ed797e1fd4acf6a3fa31

Fix duplicate parameter bug in search() recursive call
Fixed issue https://github.com/python-caldav/caldav/issues/587 where the recursive search() call had multiple bugs:
1. Used invalid syntax *kwargs2 instead of **kwargs2 to unpack dict
2. Passed sort_reverse both explicitly and in kwargs2 dict

This caused TypeError: Calendar.search() got multiple values for
argument 'sort_keys' when the backward compatibility fallback code
path was triggered (when a server returns an error on searches
without explicit comp_class).

The bug was introduced in commit 507b080d during compatibility hints
refactoring and wasn't caught earlier because this code path is only
executed for specific server error conditions.

🤖 Generated with [Claude Code](https://claude.com/claude-code)

Co-Authored-By: Claude <noreply@anthropic.com>

caldav/collection.py
@@ -976,7 +976,6 @@ class Calendar(DAVObject):
             ## those arguments:
             kwargs2 = {
                 "include_completed": include_completed,
-                "sort_reverse": sort_reverse,
                 "expand": expand,
                 "server_expand": server_expand,
                 "split_expanded": split_expanded,
@@ -1012,7 +1011,7 @@ class Calendar(DAVObject):
                     return self.search(
                         sort_keys=sort_keys,
                         sort_reverse=sort_reverse,
-                        *kwargs2,
+                        **kwargs2,
                         **kwargs,
                     )
                 raise