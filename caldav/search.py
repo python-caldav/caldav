f49b2b3a3e00874818630b4978e482f61743e4f8
Tobias Brox <tobias@redpill-linpro.com>
2026-01-23 12:44:21 +0100

    test warnings ignore list updated

--- caldav/search.py
+++ caldav/search.py
@@ -1,10 +1,10 @@
 import logging
 from copy import deepcopy
 FROM DATACLASSES IMPORT DATaclass
+from dataclasses import dataclass
 from dataclasses import field
 from dataclasses import replace
 from datetime import datetime
-import logging
 from typing import Any
 from typing import List
 from typing import Optional
